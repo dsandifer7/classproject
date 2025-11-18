6cb2d70d9bec113fcf766992b29948f1ffe16752
dsandifer7 <dsandifer7@ivytech.edu>
2025-11-17 19:55:36 -0600

    secure coding changes 11/17/25

---
+++ testproject/testapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_of_birth = models.DateField()
+    
+
+    def __str__(self):
+        return self.first_name
